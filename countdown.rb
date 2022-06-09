2c8e639111a5f5ac3f2b20be1bf86bc71827fa04
timble <timblemichael20@gmail.com>
2022-06-09 19:15:53 +1000

    complete

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,29 @@
 #write your code here
 
-def countdown
+
+def countdown(integer)
+    while integer >= 1
+        puts "#{integer} SECOND(S)!" 
+        integer -= 1
+    end
+
+    return "HAPPY NEW YEAR!"
+
 end
+
+def countdown_with_sleep(number)
+    while number >= 1
+        puts "#{number}"
+        number -= 1
+        sleep 1
+    end
+    
+
+end
+
+
+
+
+
+
+
